8e57ae0395ba3333988293f8f17799b63cb0b174
Iryna <irama05@outlook.com>
2024-01-09 20:56:31 +0200

    Implement querying frequency of word in files

--- query.py
+++ query.py
@@ -1,9 +1,25 @@
 import sys
+import os
+import importlib
 from utils import process_arguments
 
 
+# Write to standard output the list of files containing the word ordered by the number of occurrences
+# of the word in the file. Each line of output is a frequency followed by the filename
 def query_index(word, files_names, index_file_name):
-    print('Query index')
+    module = importlib.import_module(os.path.splitext(index_file_name)[0])
+    index = module.index
+    try:
+        frequency = index[word]
+    except KeyError:
+        print('Word is not present in files')
+        return
+    for i, fr in enumerate(frequency):
+        frequency[i] = str(fr) + ' ' + os.path.splitext(files_names[i])[0]
+    frequency.sort(reverse=True)
+    print('Frequency for word ' + word + ':')
+    for fr in frequency:
+        print(fr)
 
 
 if __name__ == '__main__':
